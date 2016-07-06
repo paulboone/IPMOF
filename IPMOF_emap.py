897f265c4402ddc7e9d1caa645dd805412dcfc0d
Kutay B. Sezginel <kbs37@pitt.edu>
2016-07-06 13:51:49 -0400

    Separated main IPMOF algorithm and isolated energy map generation part.
    - With job server implementation each worker will be assigned to generate an
    energy map for a given MOF combination.
    - The atom list for the energy maps will be a default list consisting of all the unique
    atom types in CoRE database to avoid generating energy map more than once.

---
+++ IPMOF_emap.py
@@ -0,0 +1,63 @@
+import os
+import math
+
+# Load 3rd party libraries
+import yaml
+
+# Load interpenetration python libraries
+from ipmof.crystal import MOF
+from ipmof.forcefield import read_ff_parameters
+from ipmof.energymap import energy_map, get_mof_list, get_mof_file_list, get_uniq_atom_list
+from ipmof.interpenetration import run_interpenetration, check_extension, save_extension
+from ipmof.core import core_mof_properties, core_mof_sort, core_mof_dir
+from ipmof.parameters import export_init_txt, export_summary_txt
+from ipmof.parameters import sim_dir_data as sim_dir    # Import simulation directories
+from ipmof.parameters import sim_par_data as sim_par    # Import simulation parameters
+# --------------------------------------------------------------------------------------------------
+# Read excel file containing force field information
+force_field = read_ff_parameters(sim_dir['force_field_path'], sim_par['force_field'])
+
+# Read MOF list from CoRE or from a given directory
+if sim_par['core_database']:
+    # Create MOf list from CoRE database
+    mof_properties = core_mof_properties(sim_dir['core_path'])
+    sorted_mofs = core_mof_sort(mof_properties, sort='void_fraction', limit=0.85)
+    mol2_list = core_mof_dir(sorted_mofs, sim_dir['mol2_dir'])
+    mof_list = get_mof_list(mol2_list, force_field)
+else:
+    # Create MOF list by reading mol2 files from a directory
+    mof_list = get_mof_file_list(sim_dir['mol2_dir'], 'mol2', force_field)
+
+# Export initialization file containing MOF names and simulation parameters
+export_init_txt(mof_list)
+# --------------------------------------------------------------------------------------------------
+# Main Loop (Energy Map) ---------------------------------------------------------------------------
+for base_mof_index, base_mof in enumerate(mof_list):
+
+    print('-----------------------------------------------------------------------------------')
+    print(base_mof_index, 'Base ->', base_mof.name)
+    print('-----------------------------------------------------------------------------------')
+
+    # Calculate atom list for remaining MOFs
+    atom_list = get_uniq_atom_list(mof_list[base_mof_index:])
+
+    # Extend base MOF for energy map calculation
+    extended_structure = base_mof.extend_unit_cell(sim_par['cut_off'])
+
+    # Calculate energy map -------------------------------------------------------------------------
+    emap = energy_map(sim_par, base_mof, atom_list)
+
+    emap_file_path = os.path.join(os.getcwd(), 'emap.yaml')
+    emap_file = open(emap_file_path, 'w')
+
+    for emap_line in emap:
+        new_line = ''
+        new_list = []
+        for emap_value in emap_line:
+            new_value = round(emap_value, 3)
+            new_line += str(emap_value) + '\t'
+            new_list.append(new_value)
+        print(new_line[:-1])
+        yaml.dump(new_list, emap_file)
+
+    emap_file.close()
